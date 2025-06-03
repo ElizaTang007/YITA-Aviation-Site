fd31050cf1909626fb815e8012bb1256d1b1cd44
ElizaTang007 <133659578+ElizaTang007@users.noreply.github.com>
2025-06-04 01:58:14 +1000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  appDir: true,  
-}
+  experimental: {
+    appDir: true,  
+};
 
-export default nextConfig
+export default nextConfig;
+
